e5d908a540eccabd2e147f400b6803a9d13ba8c9
icaroboaventura <boaventura@outlook.de>
2024-02-26 12:28:41 -0300

    Add language and sortBy

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,34 @@
 const searchBtn = document.querySelector(".btn");
+const selectLanguages = document.querySelector(".languages");
+const selectSorts = document.querySelector(".sort-by");
 
-const languages = ["Arabic", "German", "English", "Spanish", "French", "Hebrew", "Italian", "Dutch", "Portuguese", "Russian", "Swedish", "Turkish", "Chinese"];
+const languages = ["English", "Arabic", "German", "Spanish", "French", "Hebrew", "Italian", "Dutch", "Norwegian", "Portuguese", "Russian", "Swedish", "Turkish", "Chinese"];
 
-languagesCodes = ["ar", "de", "en", "es", "fr", "he", "it", "nl", "no", "pt", "ru", "sv", "ud", "zh"];
+const languagesCodes = ["en", "ar", "de", "es", "fr", "he", "it", "nl", "no", "pt", "ru", "sv", "ud", "zh"];
+
+const sortNames = ["Newest First", "Relevancy", "Popularity"];
+
+const sortValues = ["publishedAt", "relevancy", "popularity"];
+
+languages.map((lan, lanCodes) => {
+  selectLanguages.innerHTML += `
+  <option value="${languagesCodes[lanCodes]}">${lan}</option>
+  `;
+});
+
+sortNames.map((name, valuesName) => {
+  selectSorts.innerHTML += `
+  <option value="${sortValues[valuesName]}">${name}</option>
+  `;
+});
 
 const fechtData = () => {
   const keyword = document.querySelector(".search-news").value;
-  fetch(`https://newsapi.org/v2/everything?q=${keyword}&language=de&apiKey=72ddb9bddd534abea1becf3f48f43f73`)
+  const selectedLanguageCode = document.querySelector(".languages").value;
+  const selectedSortValue = document.querySelector(".sort-by").value;
+  console.log(selectedLanguageCode);
+  console.log(selectedSortValue);
+  fetch(`https://newsapi.org/v2/everything?q=${keyword}&language=${selectedLanguageCode}&sortBy=${selectedSortValue}&pageSize=50&apiKey=72ddb9bddd534abea1becf3f48f43f73`)
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
